5afdf796b9bd283d9d5e47e7c3dbcdcc81e97816
Stefan Zier <szier@icloud.com>
2017-04-15 09:34:29 -0700

    Provider's Link

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -39,7 +39,7 @@ var sendNotice = (emails, store, item) => {
 
   nodemailerMailgun.sendMail({
     from: 'notice@foodbox.com',
-      to: results,
+      to: 'spanneerselvam@scu.edu',
     subject: `${store.name} has an update for you!`,
     'h:Reply-To': 'noreply@foodbox.com',
     template: 'notice_body',
